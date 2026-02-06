6eb2c02cb89a13c6e3ced457e69802f893223e96
Kretaceous <eshan.esmail04@gmail.com>
2026-02-05 21:17:17 -0800

    Controls in Menu
    Added controls to the menu, and fixed the title's typo

--- Arcane Redemption/Assets/Scripts/MainMenu.cs
+++ Arcane Redemption/Assets/Scripts/MainMenu.cs
@@ -5,20 +5,43 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public Button playButton;
+    public Button controlsButton;
     [SerializeField] GameObject LoadingUI;
+    [SerializeField] GameObject Controls;
     private bool teleporting = false;
 
-	void Start () {
+	void Start () 
+    {
         playButton.GetComponent<Button>().onClick.AddListener(OnPlayClick);
+        controlsButton.GetComponent<Button>().onClick.AddListener(OnControlsClick);
 	}
 
+    void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Controls.activeSelf)
+            {
+                Controls.SetActive(false);
+            }
+        }
+    }
+
 	void OnPlayClick(){
-		Debug.Log ("You have clicked the button!");
+		Debug.Log ("Play Button Clicked");
 
 		// Show Loading Screen
         LoadingUI.SetActive(true);
 
 		Invoke(nameof(Teleport), 1.5f);
+	}
+
+    void OnControlsClick() 
+    {
+		Debug.Log ("Controls Button Clicked");
+
+		// Show Controls Screen
+        Controls.SetActive(true);
 	}
 
     void Teleport()

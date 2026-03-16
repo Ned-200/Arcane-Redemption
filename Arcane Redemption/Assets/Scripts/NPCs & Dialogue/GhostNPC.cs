50459c7158924956299e6e6b265f9fe1e6033ae4
Kretaceous <eshan.esmail04@gmail.com>
2026-03-15 17:18:28 -0700

    Ghost NPC Fixes
    Ghost NPC was broken after dialogue revamp, fixed it rq.

--- Arcane Redemption/Assets/Scripts/NPCs & Dialogue/GhostNPC.cs
+++ Arcane Redemption/Assets/Scripts/NPCs & Dialogue/GhostNPC.cs
@@ -7,29 +7,12 @@ using Unity.Cinemachine;
 
 public class GhostNPC : NPC_Character
 {
-    private GameObject NPC_Mesh;
     [SerializeField] protected int mayorIndex;
     private GameObject mayorCamera;
+    private bool movedOrMoving;
     protected override void Start()
     {
         base.Start();
-
-        NPC_Mesh = gameObject.transform.Find("NPC_Mesh").gameObject;
-        if (NPC_Mesh == null)
-        {
-            Debug.LogError("GhostNPC: Can't find NPC mesh gameobject");
-        }
-
-        playerController = player.GetComponent<PlayerController>();
-        if (player.transform.Find("PlayerMesh").gameObject)
-        {
-            playerMesh = player.transform.Find("PlayerMesh").gameObject;
-            weaponMesh = player.transform.Find("WeaponSlot").gameObject;
-        
-        } else
-        {
-            Debug.Log("NPC Could Not Find/Hide Player Mesh!");
-        }
 
         Invoke(nameof(BeginGhostDialogue), 3);
     }
@@ -56,64 +39,20 @@ public class GhostNPC : NPC_Character
 
     protected override void Update()
     {
+        base.Update();
 
         if (Input.GetMouseButtonDown(0) & NPC_Speaking)
         {
-            if (textComponent.text == lines[index])
+            // If cutscene index, make the Ghost rise as the camera pans up
+            if (index == cutsceneLine[cutsceneIndex] && !movedOrMoving)
             {
-                NextLine();
-
-                if (index == cutsceneLine[cutsceneIndex])
-                {
-                    StartCoroutine(TweenPosition(NPC_Mesh, new Vector3(NPC_Mesh.transform.localPosition.x, NPC_Mesh.transform.localPosition.y+8, NPC_Mesh.transform.localPosition.z), 2));
-                }
-
-            } else
-            {
-                if (TypeLineCoroutine != null) {
-                    StopCoroutine(TypeLineCoroutine);
-                }
-                textComponent.text = lines[index];
+                movedOrMoving = true;
+                StartCoroutine(TweenPosition(NPCMesh, new Vector3(NPCMesh.transform.localPosition.x, NPCMesh.transform.localPosition.y+8, NPCMesh.transform.localPosition.z), 2));
             }
-        }
-    }
-
-    protected override void NextLine()
-    {
-        if (index < lines.Length - 1)
-        {
-            index++;
-            textComponent.text = string.Empty;
-            TypeLineCoroutine = StartCoroutine(TypeLine());
-
-            Debug.Log(cutsceneIndex);
-
-            // If a cutscene camera exists, enable it on the right line
-            if (cutsceneLine.Length > 0) {
-                if (index == cutsceneLine[cutsceneIndex]) // If current line is a cutscene line enable cutscene camera
-                {
-                    cutsceneCamera[cutsceneIndex].SetActive(true);
-                    if (cutsceneIndex < cutsceneCamera.Length - 1) // only go to next cutscene index if there is more (it breaks otherwise!)
-                    {       
-                        cutsceneIndex++;
-                    }
-                }
-            }
-
-            // If a cutscene camera exists, disable it on the right line
-            if (index == endCutsceneLine) {
-                foreach (GameObject cam in cutsceneCamera)
-                {
-                    if (cam != null)
-                    {
-                        cam.SetActive(false);
-                    }
-                }
-            }
-
+            // If index is the index where the camera must pan to the mayor, do that
             if (index == mayorIndex && mayorIndex != 0)
             {
-                MeshRenderer meshRenderer = NPC_Mesh.GetComponent<MeshRenderer>();
+                MeshRenderer meshRenderer = NPCMesh.GetComponent<MeshRenderer>();
                 if (meshRenderer != null)
                 {
                     meshRenderer.enabled = false;
@@ -131,49 +70,6 @@ public class GhostNPC : NPC_Character
                     Debug.LogError("GhostNPC: Can't find mayorCamera gameobject!");
                 }
             }
-
-        } else
-        {
-            // Change Dialogue if player speaks with NPC again  
-            if (hasSecondaryLines) {
-                System.Array.Resize(ref lines, secondaryLines.Length);
-                lines = secondaryLines;
-            }
-
-            // End dialogue
-            Debug.Log("Dialogue End");
-            playerInRange = false;   
-            DialogueBox.SetActive(false);
-            CinemachineCamera.SetActive(false);
-            NPC_Speaking = false;
-
-            if (mayorCamera != null) // last line
-            {
-                mayorCamera.SetActive(false);
-            }
-
-            // Disable cutscene camera at the end of dialogue if it exists
-            foreach (GameObject cam in cutsceneCamera)
-            {
-                if (cam != null)
-                {
-                    cam.SetActive(false);
-                }
-            }
-
-            // Since dialogue is changed, remove disable the cutscene camera references. AFTER camera is deactivated!
-            if (hasSecondaryLines) {
-                cutsceneCamera = new GameObject[0];
-                cutsceneLine = new int[0];
-                endCutsceneLine = -1;
-            }
-
-            // Re-enable player movement
-            playerController.canMove = true;
-            // Un-hide player mesh
-            playerMesh.SetActive(true);
-            weaponMesh.SetActive(true);
-        
         }
     }
 
